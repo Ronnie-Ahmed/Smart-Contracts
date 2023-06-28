53872beafc43e42387fac1d6f9232becc26b1f6c
Ronnie-Ahmed <rksraisul@gmail.com>
2023-06-28 10:54:05 +0600

    newfiles

---
+++ Identity/deploy.js
@@ -0,0 +1,13 @@
+const { ethers } = require("ethers");
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../.env") });
+const { contractabi, address } = require("./constant.js");
+const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+
+const Identity = async () => {
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(address, contractabi, signer);
+  console.log("IdentityContract", contract);
+};
+Identity();
