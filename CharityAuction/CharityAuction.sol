15eef167f8c051b2380a82bc33dacb077fd589d9
Ronnie-Ahmed <rksraisul@gmail.com>
2023-08-22 19:42:48 +0600

    Crowdfunding

--- CharityAuction/CharityAuction.sol
+++ CharityAuction/CharityAuction.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "@openzeppelin/contracts@4.9.0/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@4.9.0/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts@4.9.0/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts@4.9.0/utils/Counters.sol";
-import "@openzeppelin/contracts@4.9.0/security/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 contract CharityAuction is ERC721URIStorage,Pausable{
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
