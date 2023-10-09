aaae03bbcd0c13ed6a42c8e474674e710ba469e3
Ronnie-Ahmed <rksraisul@gmail.com>
2023-10-09 22:25:25 +0600

    Solidity Tutorial

--- Solidity_Tutorial/Tutorial.sol
+++ Solidity_Tutorial/Tutorial.sol
@@ -201,3 +201,82 @@ contract HashFunction {
         return keccak256(abi.encode(text1, text2));
     }
 }
+
+contract Verify {
+    function verify(
+        address _signer,
+        string memory _message,
+        bytes memory _sig
+    ) external pure returns (bool) {
+        bytes32 hashMessage = messageHash(_message);
+        bytes32 hashSig = getEthSignedHash(hashMessage);
+        return recoverSig(hashSig, _sig) == _signer;
+    }
+
+    function messageHash(string memory _message) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_message));
+    }
+
+    function getEthSignedHash(bytes32 _hash) public pure returns (bytes32) {
+        return
+            keccak256(
+                abi.encodePacked("\x19Ethereum Signed Message:\n32", _hash)
+            );
+    }
+
+    function recoverSig(
+        bytes32 _ethSignedMessageHash,
+        bytes memory _signature
+    ) public pure returns (address) {
+        (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
+        return ecrecover(_ethSignedMessageHash, v, r, s);
+    }
+
+    function splitSignature(
+        bytes memory sig
+    ) internal pure returns (bytes32 r, bytes32 s, uint8 v) {
+        require(sig.length == 65, "invalid signature length");
+
+        assembly {
+            /*
+            First 32 bytes stores the length of the signature
+
+            add(sig, 32) = pointer of sig + 32
+            effectively, skips first 32 bytes of signature
+
+            mload(p) loads next 32 bytes starting at the memory address p into memory
+            */
+
+            // first 32 bytes, after the length prefix
+            r := mload(add(sig, 32))
+            // second 32 bytes
+            s := mload(add(sig, 64))
+            // final byte (first byte of the next 32 bytes)
+            v := byte(0, mload(add(sig, 96)))
+        }
+
+        // implicitly return (r, s, v)
+    }
+    /* Signature Verification
+
+How to Sign and Verify
+# Signing
+1. Create message to sign
+2. Hash the message
+3. Sign the hash (off chain, keep your private key secret)
+
+# Verify
+1. Recreate hash from the original message
+2. Recover signer from signature and hash
+3. Compare recovered signer to claimed signer
+
+
+
+ethereum.enable()
+account="0x..."
+hash=" " messageHash
+ethereum.request({ method: "personal_sign", params: [account, hash]}).then(console.log)
+will return bytes memory _sig
+     
+*/
+}
